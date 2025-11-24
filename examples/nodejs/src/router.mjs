3ec27c99ceb10fd3c296f3d7115bddd836df3d08
Zack Cheng <zack_cheng@tech.gov.sg>
2025-11-24 11:39:19 +0800

    feat: improve singpassClient initialization and explanation

--- examples/nodejs/src/router.mjs
+++ examples/nodejs/src/router.mjs
@@ -3,55 +3,51 @@ import { Issuer, generators, custom } from 'openid-client';
 import * as crypto from 'crypto';
 import Router from '@koa/router';
 
-// This demo uses panva/node-openid-client, an off-the-shelf OIDC client.
+// This demo uses panva/node-openid-client, an off-the-shelf OIDC client library.
 
-const singpassIssuer = await Issuer.discover(config.ISSUER_URL);
+let singpassClient;
+await initializeSingpassClient();
 
-let singpassClient = new singpassIssuer.Client(
-  {
-    // All the hardcoded values used below are taken from Singpass' OpenID Provider Metadata,
-    // which can be found at config.ISSUER_URL + '/.well-known/openid-configuration'
-    client_id: config.CLIENT_ID,
-    response_types: ['code'],
-    token_endpoint_auth_method: 'private_key_jwt',
-    id_token_signed_response_alg: 'ES256',
-    userinfo_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
-    userinfo_encrypted_response_enc: 'A256GCM',
-    userinfo_signed_response_alg: 'ES256',
-    // The following two fields are only required if the client's (RP) profile is 'direct_pii_allowed'.
-    // id_token_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
-    // id_token_encrypted_response_enc: 'A256CBC-HS512',
-  },
-  { keys: [config.KEYS.PRIVATE_SIG_KEY, config.KEYS.PRIVATE_ENC_KEY] }
-);
-
-async function refreshIssuerConfig() {
+/**
+ * Initializes the Singpass OIDC client. This is mostly done automatically, based on Singpass' OpenID Provider config,
+ * which can be retrieved as a JSON document from `<issuer_url>/.well-known/openid-configuration`, but some manual
+ * configuration is necessary depending on the client library you're using.
+ */
+async function initializeSingpassClient() {
   try {
-    const newIssuerConfig = await Issuer.discover(config.ISSUER_URL);
-    singpassClient = new newIssuerConfig.Client({
-      client_id: config.CLIENT_ID,
-      response_types: ['code'],
-      token_endpoint_auth_method: 'private_key_jwt',
-      id_token_signed_response_alg: 'ES256',
-      userinfo_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
-      userinfo_encrypted_response_enc: 'A256GCM',
-      userinfo_signed_response_alg: config.KEYS.PRIVATE_SIG_KEY.alg,
-    }, { 
-      keys: [config.KEYS.PRIVATE_SIG_KEY, config.KEYS.PRIVATE_ENC_KEY] 
-    });
-
+    const singpassIssuer = await Issuer.discover(config.ISSUER_URL);
+    singpassClient = new singpassIssuer.Client(
+      {
+        client_id: config.CLIENT_ID,
+        // This OIDC client library requires that you manually configure its behaviour via the options below.
+        // Please refer to Singpass' OpenID Provider config, which specifies the supported values for each field, or
+        // to https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata, for info about each field.
+        // For most fields, Singpass supports just one valid value, so you likely won't have to change this very much.
+        response_types: ['code'],
+        token_endpoint_auth_method: 'private_key_jwt',
+        id_token_signed_response_alg: 'ES256',
+        // The following options should be included if and only if your app is a Myinfo app.
+        userinfo_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
+        userinfo_encrypted_response_enc: 'A256GCM',
+        userinfo_signed_response_alg: 'ES256',
+        // The following options should be included if and only if your app is configured (in the Singpass Developer
+        // Portal) to receive encrypted ID token responses (i.e. your app's client profile is 'direct_pii_allowed').
+        // id_token_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
+        // id_token_encrypted_response_enc: 'A256CBC-HS512',
+      },
+      {
+        keys: [config.KEYS.PRIVATE_SIG_KEY, config.KEYS.PRIVATE_ENC_KEY],
+      }
+    );
   } catch (error) {
-    console.error('Failed to refresh issuer configuration:', error);
+    console.error('Failed to initialize Singpass client', error);
   }
 }
 
-// Recommended to refresh the issuer as opposed to caching indefinitely
+// To keep your Singpass OIDC client up-to-date with any changes to Singpass' OIDC config, we recommend reinitializing
+// it periodically (an hour or more apart). This pulls the latest OIDC config and updates the client accordingly.
 const ONE_HOUR_IN_MS = 60 * 60 * 1000;
-setInterval(refreshIssuerConfig, ONE_HOUR_IN_MS);
-
-custom.setHttpOptionsDefaults({
-  timeout: 15000,
-});
+setInterval(initializeSingpassClient, ONE_HOUR_IN_MS);
 
 // This demo uses Koa for routing.
 
@@ -94,7 +90,7 @@ router.get('/callback', async function handleSingpassCallback(ctx) {
     console.log('These are the claims in the ID token:');
     console.log(tokenSet.claims());
 
-    // Userinfo request (available only to apps with additional allowed scopes, beyond just 'openid').
+    // Userinfo request (this is only necessary if your app is a Myinfo app)
     const userInfo = await singpassClient.userinfo(tokenSet);
     console.log('This is the user info returned:');
     console.log(userInfo);
