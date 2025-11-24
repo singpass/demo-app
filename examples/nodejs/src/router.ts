fe83c57c146d6f5b533fd4af8c875d51a54f0abf
Zack Cheng <zack_cheng@tech.gov.sg>
2025-11-24 12:23:46 +0800

    feat: upgrade openid client from v5 to v6

--- examples/nodejs/src/router.ts
+++ examples/nodejs/src/router.ts
@@ -1,53 +1,66 @@
 import config from '../../config.json';
-import { Issuer, generators, custom, BaseClient } from 'openid-client';
-import * as crypto from 'crypto';
+import * as openidClient from 'openid-client';
 import Router from '@koa/router';
 
-// This demo uses panva/node-openid-client, an off-the-shelf OIDC client library.
+// This demo uses panva/openid-client, an off-the-shelf OIDC client library.
 
-let singpassClient: BaseClient;
-await initializeSingpassClient();
+// Keys imported using crypto.subtle.importKey do not retain the 'kid' nor 'alg' properties, but both are needed
+// for this OIDC client library (e.g. to pick the correct decryption key to use), so we provide them here
+
+const privateSigningKey = {
+  kid: config.KEYS.PRIVATE_SIG_KEY.kid,
+  alg: config.KEYS.PRIVATE_SIG_KEY.alg,
+  key: await crypto.subtle.importKey(
+    'jwk',
+    config.KEYS.PRIVATE_SIG_KEY,
+    { name: 'ECDSA', namedCurve: 'P-256' },
+    false,
+    ['sign']
+  ),
+};
+
+const privateEncryptionKey = {
+  kid: config.KEYS.PRIVATE_ENC_KEY.kid,
+  alg: config.KEYS.PRIVATE_ENC_KEY.alg,
+  key: await crypto.subtle.importKey('jwk', config.KEYS.PRIVATE_ENC_KEY, { name: 'ECDH', namedCurve: 'P-256' }, false, [
+    'deriveKey',
+    'deriveBits',
+  ]),
+};
+
+let singpassConfig: openidClient.Configuration;
+await initializeSingpassConfig();
 
 /**
  * Initializes the Singpass OIDC client. This is mostly done automatically, based on Singpass' OpenID Provider config,
  * which can be retrieved as a JSON document from `<issuer_url>/.well-known/openid-configuration`, but some manual
  * configuration is necessary depending on the client library you're using.
  */
-async function initializeSingpassClient() {
+async function initializeSingpassConfig() {
   try {
-    const singpassIssuer = await Issuer.discover(config.ISSUER_URL);
-    singpassClient = new singpassIssuer.Client(
-      {
-        client_id: config.CLIENT_ID,
-        // This OIDC client library requires that you manually configure its behaviour via the options below.
-        // Please refer to Singpass' OpenID Provider config, which specifies the supported values for each field, or
-        // to https://openid.net/specs/openid-connect-discovery-1_0.html#ProviderMetadata, for info about each field.
-        // For most fields, Singpass supports just one valid value, so you likely won't have to change this very much.
-        response_types: ['code'],
-        token_endpoint_auth_method: 'private_key_jwt',
-        id_token_signed_response_alg: 'ES256',
-        // The following options should be included if and only if your app is a Myinfo app.
-        userinfo_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
-        userinfo_encrypted_response_enc: 'A256GCM',
-        userinfo_signed_response_alg: 'ES256',
-        // The following options should be included if and only if your app is configured (in the Singpass Developer
-        // Portal) to receive encrypted ID token responses (i.e. your app's client profile is 'direct_pii_allowed').
-        // id_token_encrypted_response_alg: config.KEYS.PRIVATE_ENC_KEY.alg,
-        // id_token_encrypted_response_enc: 'A256CBC-HS512',
-      },
-      {
-        keys: [config.KEYS.PRIVATE_SIG_KEY, config.KEYS.PRIVATE_ENC_KEY],
-      }
+    singpassConfig = await openidClient.discovery(
+      new URL(config.ISSUER_URL),
+      config.CLIENT_ID,
+      undefined,
+      openidClient.PrivateKeyJwt(privateSigningKey)
+    );
+    openidClient.enableDecryptingResponses(
+      singpassConfig,
+      [
+        'A256GCM', //       encrypted userinfo responses are encrypted using this algorithm
+        'A256CBC-HS512', // encrypted ID token responses are encrypted using this algorithm
+      ],
+      privateEncryptionKey
     );
   } catch (error) {
-    console.error('Failed to initialize Singpass client', error);
+    console.error('Failed to initialize Singpass config', error);
   }
 }
 
 // To keep your Singpass OIDC client up-to-date with any changes to Singpass' OIDC config, we recommend reinitializing
 // it periodically (an hour or more apart). This pulls the latest OIDC config and updates the client accordingly.
 const ONE_HOUR_IN_MS = 60 * 60 * 1000;
-setInterval(initializeSingpassClient, ONE_HOUR_IN_MS);
+setInterval(initializeSingpassConfig, ONE_HOUR_IN_MS);
 
 // This demo uses Koa for routing.
 
@@ -58,14 +71,14 @@ router.get('/.well-known/jwks.json', function getJwks(ctx) {
 });
 
 router.get('/login', async function handleLogin(ctx) {
-  const code_verifier = generators.codeVerifier();
-  const code_challenge = generators.codeChallenge(code_verifier);
-  const nonce = crypto.randomUUID();
-  const state = crypto.randomBytes(16).toString('hex');
+  const code_verifier = openidClient.randomPKCECodeVerifier();
+  const code_challenge = await openidClient.calculatePKCECodeChallenge(code_verifier);
+  const nonce = openidClient.randomNonce();
+  const state = openidClient.randomState();
   ctx.session.auth = { code_verifier, nonce, state };
 
   // Authorization request
-  const authorizationUrl = singpassClient.authorizationUrl({
+  const redirectTo = openidClient.buildAuthorizationUrl(singpassConfig, {
     redirect_uri: config.REDIRECT_URI,
     code_challenge_method: 'S256',
     code_challenge,
@@ -73,29 +86,35 @@ router.get('/login', async function handleLogin(ctx) {
     state,
     scope: config.SCOPES,
   });
-  ctx.redirect(authorizationUrl);
+  ctx.redirect(redirectTo.href);
 });
 
 router.get('/callback', async function handleSingpassCallback(ctx) {
   try {
-    const receivedQueryParams = ctx.request.query;
+    const currentUrl = new URL(ctx.request.href);
     const { code_verifier, nonce, state } = ctx.session.auth;
 
     // Token request
-    const tokenSet = await singpassClient.callback(config.REDIRECT_URI, receivedQueryParams, {
-      code_verifier,
-      nonce,
-      state,
+    const tokens = await openidClient.authorizationCodeGrant(singpassConfig, currentUrl, {
+      pkceCodeVerifier: code_verifier,
+      expectedNonce: nonce,
+      expectedState: state,
+      idTokenExpected: true,
     });
+    const idTokenClaims = tokens.claims();
     console.log('These are the claims in the ID token:');
-    console.log(tokenSet.claims());
+    console.log(idTokenClaims);
 
-    // Userinfo request (this is only necessary if your app is a Myinfo app)
-    const userInfo = await singpassClient.userinfo(tokenSet);
+    if (idTokenClaims === undefined) {
+      throw new Error('ID token claims are undefined');
+    }
+
+    // Userinfo request (this is only necessary if your app is a Myinfo app).
+    const userInfo = await openidClient.fetchUserInfo(singpassConfig, tokens.access_token, idTokenClaims.sub);
     console.log('This is the user info returned:');
     console.log(userInfo);
 
-    ctx.session.user = { ...tokenSet.claims(), ...userInfo };
+    ctx.session.user = { ...idTokenClaims, ...userInfo };
     ctx.redirect('/');
   } catch (err) {
     console.error(err);
